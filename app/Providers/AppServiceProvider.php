cc787ffb55ee65cf2889b4ad42fd21cec2311203
goginyan <arsengoginian@gmail.com>
2020-08-13 03:09:17 +0400

    added morph map for morphing models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,19 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 use Spatie\QueryBuilder\QueryBuilderRequest;
+use App\Models\Clinicaltrial;
+use App\Models\JobApplication;
+use App\Models\Job;
+use App\Models\Event;
+use App\Models\Focus;
+use App\Models\Location;
+use App\Models\Investor;
+use App\Models\Research;
+use App\Models\Person;
+use App\Models\Company;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +36,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         QueryBuilderRequest::setArrayValueDelimiter('|');
+        Relation::morphMap([
+            'organization'   => Company::class,
+            'person'         => Person::class,
+            'research'       => Research::class,
+            'investor'       => Investor::class,
+            'location'       => Location::class,
+            'focus'          => Focus::class,
+            'event'          => Event::class,
+            'job'            => Job::class,
+            'apply'          => JobApplication::class,
+            'clinical-trial' => Clinicaltrial::class,
+        ]);
     }
 }
