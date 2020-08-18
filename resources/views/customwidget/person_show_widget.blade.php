9c1a923c0a8282479718fd4fad421fffcdd8b16e
Sydney Alcala <sydney@gotsmith.com>
2020-08-18 12:53:05 -0500

    Add Clinical Trials to Backpack Person show widget

--- resources/views/customwidget/person_show_widget.blade.php
+++ resources/views/customwidget/person_show_widget.blade.php
@@ -4,13 +4,10 @@
 
         <div class="col-12 col-md-8 col-xl-4 d-flex">
             <div class="card card-body flex-fill">
-
                 <div class="row mb-2">
                     <div class="col"><h5 class="mb-1">Organizations</h5></div>
                     <div class="col text-right">
                         <a href="/admin/person/{{ $widget['person']->id }}/company" class="btn btn-sm btn-primary font-weight-bold">Add <i class='nav-icon la la-user'></i></a>
-
-                        {{-- <button class="btn btn-link p-0 load-ajax-modal text-left" data-title="{{ $widget['company']->name }}" data-path="/admin/companyperson/{{ $widget['company']->id }}" data-toggle="modal" data-target="#dynamic-modal">{{ $widget['company']->name }}</button> --}}
                     </div>
                 </div>
 
@@ -30,15 +27,12 @@
             </div>
         </div>
 
-
         <div class="col-12 col-md-8 col-xl-4 d-flex">
             <div class="card card-body flex-fill">
                 <div class="row mb-2">
                     <div class="col"><h5 class="mb-1">Investors</h5></div>
                     <div class="col text-right">
                         <a href="/admin/person/{{ $widget['person']->id }}/investor" class="btn btn-sm btn-primary font-weight-bold">Add <i class='nav-icon la la-user'></i></a>
-
-                        {{-- <button class="btn btn-link p-0 load-ajax-modal text-left" data-title="{{ $widget['company']->name }}" data-path="/admin/companyperson/{{ $widget['company']->id }}" data-toggle="modal" data-target="#dynamic-modal">{{ $widget['company']->name }}</button> --}}
                     </div>
                 </div>
 
@@ -58,6 +52,34 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-12 col-md-8 col-xl-4 d-flex">
+            <div class="card card-body flex-fill">
+                <div class="row mb-0">
+                    <div class="col"><h5 class="mb-1">Clinical Trials</h5></div>
+                    <div class="col text-right">
+                        {{-- <a href="/admin/person/{{ $widget['person']->id }}/company" class="btn btn-sm btn-primary font-weight-bold">Add <i class='nav-icon la la-user'></i></a> --}}
+                    </div>
+                </div>
+
+                <div class="list-group list-group-flush">
+                    @forelse ($widget['person']['clinicaltrials'] as $clinicaltrial)
+                        <div class="list-group-item d-flex justify-content-between">
+                            <a href="/admin/clinicaltrial/{{ $clinicaltrial->id }}/show">
+                                {{ $clinicaltrial->title }}
+                            </a>
+                            {{-- <a class="small" onclick="return confirm_action()" href="{{ route('companyperson.remove', ['company_id' => $company->id, 'person_id' => $widget['person']->id]) }}">
+                                <i class="la la-trash"></i> Remove
+                            </a> --}}
+                        </div>
+                    @empty
+                        -
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script>
@@ -65,3 +87,8 @@
         return confirm('are you sure?');
     }
 </script>
+<style>
+    .list-group-flush .list-group-item:first-child {
+        border-top-width: 0;
+    }
+</style>
