f1939c7356c592f14548d4845d0350eacd9b64dc
Sydney Alcala <sydney@gotsmith.com>
2020-08-18 14:59:05 -0500

    Updating UI in Backpack - Organization show view

--- resources/views/customwidget/company_show_widget.blade.php
+++ resources/views/customwidget/company_show_widget.blade.php
@@ -1,6 +1,6 @@
-<div class="{{ $widget['class'] ?? 'well mb-2' }} mt-4">
+<div class="{{ $widget['class'] ?? 'well mb-2' }} mt-4 col-xl-8 p-0">
 	<div class="row">
-		<div class="col-12 col-md-8 col-xl-4 d-flex">
+		<div class="col-12 col-md-8 col-xl-6 d-flex">
 			<div class="card card-body flex-fill">
 				<div>
 					@if($widget['company']->logo != '')
@@ -26,21 +26,23 @@
 			</div>
 		</div>
 
-		<div class="col-12 col-md-8 col-xl-4 d-flex">
-			<div class="card card-body flex-fill">
-				<h5 class="mb-1">Location</h5>
-		
-				@forelse ($widget['company']['locations'] as $location)
-					<a href="/admin/location/{{ $location->id }}/show">{{ $location->name }}</a>@if (!$loop->last) <br> @endif
-				@empty
-					-
-				@endforelse
+		@if ($widget['company']['locations']->count() > 0)
+			<div class="col-12 col-md-8 col-xl-6 d-flex">
+				<div class="card card-body flex-fill">
+					<h5 class="mb-1">Locations</h5>
+			
+					<div class="list-group list-group-flush">
+						@foreach ($widget['company']['locations'] as $location)
+							<div class="list-group-item">
+								<a href="/admin/location/{{ $location->id }}/show" class="d-block">{{ $location->name }}</a>
+							</div>
+						@endforeach
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
+		@endif
 
-	<div class="row">
-		<div class="col-12 col-md-8 col-xl-4 d-flex">
+		<div class="col-12 col-md-8 col-xl-6 d-flex">
 			<div class="card card-body flex-fill">
 				<div class="row mb-2">
 					<div class="col"><h5 class="mb-1">People</h5></div>
@@ -49,56 +51,110 @@
 					</div>
 				</div>
 		
-				@forelse ($widget['company']['people'] as $person)
-					<div class="d-flex justify-content-between">
-						<a href="/admin/person/{{ $person->id }}/show">
-							{{ $person->name }} ({{ $person->getOriginal('pivot_position') }})
-						</a> 
-						<a class="small" onclick="return confirm_action()" href="{{ route('companyperson.remove', ['company_id' => $widget['company']->id, 'person_id' => $person->id]) }}">
-							<i class="la la-trash"></i> Remove
-						</a>
+				<div class="list-group list-group-flush">
+					@forelse ($widget['company']['people'] as $person)
+						<div class="list-group-item d-flex justify-content-between">
+							<a href="/admin/person/{{ $person->id }}/show">
+								{{ $person->name }} ({{ $person->getOriginal('pivot_position') }})
+							</a> 
+							<a class="small" onclick="return confirm_action()" href="{{ route('companyperson.remove', ['company_id' => $widget['company']->id, 'person_id' => $person->id]) }}">
+								<i class="la la-trash"></i> Remove
+							</a>
+						</div>
+					@empty
+						-
+					@endforelse
+				</div>
+			</div>
+		</div>
+
+		@if ($widget['company']['investors']->count() > 0)
+			<div class="col-12 col-md-8 col-xl-6 d-flex">
+				<div class="card card-body flex-fill">
+					<h5 class="mb-1">Investors</h5>
+			
+					<div class="list-group list-group-flush">
+						@foreach ($widget['company']['investors'] as $investor)
+							<div class="list-group-item d-flex justify-content-between">
+								<a href="/admin/investor/{{ $investor->id }}/show" class="d-block">{{ $investor->name }}</a>
+							</div>
+						@endforeach
 					</div>
-				@empty
-					-
-				@endforelse
+				</div>
 			</div>
-		</div>
+		@endif
 
-		<div class="col-12 col-md-8 col-xl-4 d-flex">
-			<div class="card card-body flex-fill">
-		
-				<h5 class="mb-1">Investors</h5>
-		
-				@forelse ($widget['company']['investors'] as $investor)
-					 <a href="/admin/investor/{{ $investor->id }}/show">{{ $investor->name }}</a> @if (!$loop->last) <br> @endif
-				@empty
-					-
-				@endforelse
+		@if ($widget['company']['clinicaltrials']->count() > 0)
+	        <div class="col-12 col-md-8 col-xl-6 d-flex">
+	            <div class="card card-body flex-fill">
+	                <h5 class="mb-0">Clinical Trials</h5>
 
-			</div>
-		</div>
-	</div>
+	                <div class="list-group list-group-flush">
+	                    @foreach ($widget['company']['clinicaltrials'] as $clinicaltrial)
+	                        <div class="list-group-item d-flex justify-content-between">
+	                            <a href="/admin/clinicaltrial/{{ $clinicaltrial->id }}/show">
+	                                {{ $clinicaltrial->title }}
+	                            </a>
+	                        </div>
+	                    @endforeach
+	                </div>
+	            </div>
+	        </div>
+        @endif
 
-	<div class="row">
-        <div class="col-12 col-md-8 col-xl-4 d-flex">
-            <div class="card card-body flex-fill">
-                <div class="row mb-0">
-                    <div class="col"><h5 class="mb-1">Clinical Trials</h5></div>
-                </div>
+		@if ($widget['company']['research']->count() > 0)
+	        <div class="col-12 col-md-8 col-xl-6 d-flex">
+	            <div class="card card-body flex-fill">
+	                <h5 class="mb-0">Research</h5>
 
-                <div class="list-group list-group-flush">
-                    @forelse ($widget['company']['clinicaltrials'] as $clinicaltrial)
-                        <div class="list-group-item d-flex justify-content-between">
-                            <a href="/admin/clinicaltrial/{{ $clinicaltrial->id }}/show">
-                                {{ $clinicaltrial->title }}
-                            </a>
-                        </div>
-                    @empty
-                        -
-                    @endforelse
-                </div>
-            </div>
-        </div>
+	                <div class="list-group list-group-flush">
+	                    @foreach ($widget['company']['research'] as $item)
+	                        <div class="list-group-item d-flex justify-content-between">
+	                            <a href="/admin/research/{{ $item->id }}/show">
+	                                {{ $item->name }}
+	                            </a>
+	                        </div>
+	                    @endforeach
+	                </div>
+	            </div>
+	        </div>
+        @endif
+
+        @if ($widget['company']['events']->count() > 0)
+	        <div class="col-12 col-md-8 col-xl-6 d-flex">
+	            <div class="card card-body flex-fill">
+	                <h5 class="mb-0">Events</h5>
+
+	                <div class="list-group list-group-flush">
+	                    @foreach ($widget['company']['events'] as $event)
+	                        <div class="list-group-item d-flex justify-content-between">
+	                            <a href="/admin/event/{{ $event->id }}/show">
+	                                {{ $event->name }}
+	                            </a>
+	                        </div>
+	                    @endforeach
+	                </div>
+	            </div>
+	        </div>
+        @endif
+
+        @if ($widget['company']['jobs']->count() > 0)
+	        <div class="col-12 col-md-8 col-xl-6 d-flex">
+	            <div class="card card-body flex-fill">
+	                <h5 class="mb-0">Jobs</h5>
+
+	                <div class="list-group list-group-flush">
+	                    @foreach ($widget['company']['jobs'] as $job)
+	                        <div class="list-group-item d-flex justify-content-between">
+	                            <a href="/admin/job/{{ $job->id }}/show">
+	                                {{ $job->job_title }}
+	                            </a>
+	                        </div>
+	                    @endforeach
+	                </div>
+	            </div>
+	        </div>
+        @endif
     </div>
 </div>
 
@@ -108,6 +164,12 @@
 		width:  75px;
 		float:  right;
 	}
+	.list-group-flush .list-group-item {
+		padding: .25rem 0;
+	}
+	.list-group-flush .list-group-item:first-child {
+        border-top-width: 0;
+    }
 </style>
 
 <script>
