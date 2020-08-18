529a28c3c39cb7b3f75adadffa6b758b271a1576
Sydney Alcala <sydney@gotsmith.com>
2020-08-18 15:25:01 -0500

    Add events to Discover person show

--- resources/views/discover/people/data.blade.php
+++ resources/views/discover/people/data.blade.php
@@ -72,6 +72,17 @@
 				</ul>
 			@endif
 
+			@if($person->events->count() > 0)
+				<p class="mb-0 mt-4 h5">Events:</p>
+				<ul class="list-group list-group-flush">
+					@foreach ($person->events as $event)
+					    <li class="list-group-item px-0 py-1">
+					    	<a class="d-block" href="{{ route('discover.events.show', $event->slug) }}">{{ $event->name }}</a>
+					    </li>
+					@endforeach
+				</ul>
+			@endif
+
 			@if($person->bio != '')
 				<p class="mb-0">
 					<strong>Bio:</strong>
