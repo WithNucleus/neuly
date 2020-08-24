b1099995619a534dd41bfe7bc6e7ffbeb3d1277d
Sydney Alcala <sydney@gotsmith.com>
2020-08-24 10:09:59 -0500

    Removed icon and made label size larger

--- resources/views/sidebars/companies.blade.php
+++ resources/views/sidebars/companies.blade.php
@@ -1,7 +1,7 @@
 <div class="filter-checkbox-container mb-3" aria-label="Filter by additional options">
     <div class="custom-control custom-checkbox">
         <input type="checkbox" class="custom-control-input" name="hiring" id="hiring" value="1" @if ($filter_hiring && $filter_hiring == 1) checked @endif>
-        <label class="custom-control-label" for="hiring">Hiring now <i class="fad fa-briefcase"></i></label>
+        <label class="custom-control-label lead-smaller" for="hiring">Now Hiring</label>
     </div>
 </div>
 
