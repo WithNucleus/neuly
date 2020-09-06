5763c2d185ae0dee0f25a4afa1a31f10c7354f20
Marcel Moll <marcel@lounge-media.de>
2020-09-06 21:01:51 +0200

    changing order for mobile navigation

--- resources/views/navbars/tabs-mobile.blade.php
+++ resources/views/navbars/tabs-mobile.blade.php
@@ -7,11 +7,6 @@
                             selected
                         @endif
                             >Dashboard</option>
-            <option value="home"
-                        @if(Route::is('index') OR Route::is('home'))
-                            selected
-                        @endif
-                            >Home</option>
             <option value="organizations"
                         @if(Route::is('discover.organizations') OR Route::is('discover.organizations.show'))
                             selected
@@ -22,49 +17,46 @@
                             selected
                         @endif
                             >People</option>
+            <option value="investors"
+                    @if(Route::is('discover.investors') OR Route::is('discover.investors.show'))
+                    selected
+                @endif
+            >Investors</option>
+            <option value="locations"
+                    @if(Route::is('discover.locations') OR Route::is('discover.locations.show'))
+                    selected
+                @endif
+            >Locations</option>
+            <option value="focus"
+                    @if(Route::is('discover.focus') OR Route::is('discover.focus.show'))
+                    selected
+                @endif
+            >Focus</option>
             <option value="research"
                         @if(Route::is('discover.research') OR Route::is('discover.research.show'))
                             selected
                         @endif
                             >Research</option>
-            <option value="locations"
-                        @if(Route::is('discover.locations') OR Route::is('discover.locations.show'))
-                            selected
-                        @endif
-                            >Locations</option>
-            <option value="focus"
-                        @if(Route::is('discover.focus') OR Route::is('discover.focus.show'))
-                            selected
-                        @endif
-                            >Focus</option>
-
+            <option value="clinicaltrials"
+                    @if(Route::is('discover.clinicaltrials') OR Route::is('discover.clinicaltrials.show'))
+                    selected
+                @endif
+            >Clinical Trials</option>
+            <option value="index"
+                    @if(Route::is('discover.index'))
+                    selected
+                @endif
+            >Pubco Index</option>
             <option value="events"
                         @if(Route::is('discover.events') OR Route::is('discover.events.show') OR Route::is('discover.events.past'))
                             selected
                         @endif
                             >Events</option>
-
             <option value="jobs"
                         @if(Route::is('discover.jobs') OR Route::is('discover.jobs.show'))
                             selected
                         @endif
                             >Jobs</option>
-
-            <option value="clinicaltrials"
-                        @if(Route::is('discover.clinicaltrials') OR Route::is('discover.clinicaltrials.show'))
-                            selected
-                        @endif
-                            >Clinical Trials</option>
-            <option value="index"
-                        @if(Route::is('discover.index'))
-                            selected
-                        @endif
-                            >Pubco Index</option>
-            <option value="investors"
-                        @if(Route::is('discover.investors') OR Route::is('discover.investors.show'))
-                            selected
-                        @endif
-                            >Investors</option>
             <option value="insights"
                         @if(Route::is('discover.insights'))
                             selected
@@ -74,6 +66,12 @@
             @if(Route::is('search') OR Route::is('search.term.results') OR Route::is('search.term'))
                 <option value="search" selected>Search</option>
             @endif
+
+            <option value="home"
+                    @if(Route::is('index') OR Route::is('home'))
+                    selected
+                @endif
+            >Home</option>
         </select>
     </div>
 </form>
