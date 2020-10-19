cd8b7547916eb094d1d7c642de91cd5d0dc7b725
Sydney Alcala <sydney@gotsmith.com>
2020-10-19 17:25:00 -0500

    NEULY-184 Branding - Homepage Updates

--- resources/views/content/homepage/hero.blade.php
+++ resources/views/content/homepage/hero.blade.php
@@ -1,13 +1,44 @@
+{{--<div class="position-relative p-3 p-md-5 text-center home-hero shadow-sm">--}}
+{{--    <div class="col-md-10 col-lg-8 mx-auto my-5 text-center">--}}
+{{--        <h1 class="hero-title font-weight-normal mt-2 col-lg-8 mx-auto mb-2">Clear data to build the future of psychedelics.</h1>--}}
+{{--        <form class="search-form form-inline mx-auto justify-content-center" method="post" action="{{ route('search') }}">--}}
+{{--            @csrf--}}
+{{--            <input class="form-control hero-search-input shadow-sm search-field js-global-search-input" name="search" type="search" placeholder="Discover organizations, people, research..." aria-label="Search">--}}
+{{--            <button class="btn hero-search-button ml-2 my-2 my-sm-0 shadow-sm" type="submit">Search</button>--}}
+{{--        </form>--}}
+{{--        <p class="mt-2" style="font-size: 1.5rem;font-weight: 600">--}}
+{{--            <a href="{{ route('member.dashboard') }}" class="text-dark text-decoration-none border-bottom-dark-heavy">Or start exploring our database...</a>--}}
+{{--        </p>--}}
+{{--    </div>--}}
+{{--    <div class="col-12 col-lg-10">--}}
+{{--        <img src="{{ asset('images/home-clinical-trial-tracker.png') }}" alt="" style="max-width: 600px;height: auto;">--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+
 <div class="position-relative p-3 p-md-5 text-center home-hero shadow-sm">
-    <div class="col-md-10 col-lg-8 mx-auto my-5 text-center">
-        <h1 class="hero-title font-weight-normal mt-2 col-lg-8 mx-auto mb-2">Clear data to build the future of psychedelics.</h1>
-        <form class="search-form form-inline mx-auto justify-content-center" method="post" action="{{ route('search') }}">
-            @csrf
-            <input class="form-control hero-search-input shadow-sm search-field js-global-search-input" name="search" type="search" placeholder="Discover organizations, people, research..." aria-label="Search">
-            <button class="btn hero-search-button ml-2 my-2 my-sm-0 shadow-sm" type="submit">Search</button>
-        </form>
-        <p class="mt-2" style="font-size: 1.5rem;font-weight: 600">
-            <a href="{{ route('member.dashboard') }}" class="text-dark text-decoration-none border-bottom-dark-heavy">Or start exploring our database...</a>
-        </p>
+    <div class="container-fluid">
+        <h1 class="hero-title font-weight-normal mt-5 col-lg-8 mx-auto mb-5">Clear data to build the future of psychedelics.</h1>
+{{--        <h2 class="font-normal">Neuly collects and analyzes data across the psychedelic industry, providing you with transparent information and proprietary insights.</h2>--}}
+{{--        <img src="{{ asset('images/home-clinical-trial-tracker.png') }}" alt="" style="width: 100%;max-width: 800px;height: auto;">--}}
+        <div class="row mb-5">
+            <div class="col-12 col-md-5 col-lg-6 text-md-right pr-md-5">
+                <img src="{{ asset('images/home-clinical-trial-tracker.png') }}" alt="" style="width: 100%;max-width: 600px;height: auto;">
+            </div>
+            <div class="col-12 col-md-7 col-lg-6 text-left">
+                <h2 class="mt-xl-5 h4 font-normal font-weight-normal">Data &amp; Insights for the Psychedelics Industry</h2>
+                <ul>
+                    <li class="lead pt-2 pb-2">Neuly collects and analyzes the mountains of data</li>
+                    <li class="lead pt-2 pb-2">You browse our extensive database and insights</li>
+                    <li class="lead pt-2 pb-2">We all work together to grow the psychedelics industry</li>
+                </ul>
+                <p class="ml-md-3">
+                    <a href="{{ route('member.dashboard') }}" class="btn btn-dark btn-lg">Start exploring our database...</a>
+                </p>
+                <p class="ml-md-3">
+                    <a href="{{ route('about') }}" class="text-dark">Tell me more about Neuly</a>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/content/homepage/index.blade.php
+++ resources/views/content/homepage/index.blade.php
@@ -16,22 +16,24 @@
             </div>
         @endif
 
-        @include('content.homepage.neuly-knows')
+{{--        @include('content.homepage.neuly-knows')--}}
         @include('content.homepage.who-is-neuly-for')
 
-        <div class="container pt-2">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    @include('content.homepage.insights-widget')
-                    @include('content.homepage.news')
-                    @include('discover.insights.widgets.organizations-by-type')
-                    @include('content.homepage.recent-jobs')
-                </div>
+        <div class="container-fluid pt-2">
+            <div class="home-card-container mx-auto">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        @include('content.homepage.insights-widget')
+                        @include('content.homepage.news')
+                        @include('discover.insights.widgets.organizations-by-type')
+                        @include('content.homepage.recent-jobs')
+                    </div>
 
-                <div class="col-12 col-md-6">
-                    @include('discover.insights.widgets.companies-by-focus-drug')
-                    @include('content.homepage.upcoming-events')
-                    @include('discover.insights.widgets.top-ten-locations')
+                    <div class="col-12 col-md-6">
+                        @include('discover.insights.widgets.companies-by-focus-drug')
+                        @include('content.homepage.upcoming-events')
+                        @include('discover.insights.widgets.top-ten-locations')
+                    </div>
                 </div>
             </div>
         </div>
