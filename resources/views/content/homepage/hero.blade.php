7b49f5d9e7831be21af2aab5a5d2927ccf1c1b06
Sydney Alcala <sydney@gotsmith.com>
2020-10-21 15:50:26 -0500

    NEULY-184 Branding - Homepage Updates

--- resources/views/content/homepage/hero.blade.php
+++ resources/views/content/homepage/hero.blade.php
@@ -1,23 +1,30 @@
 <div class="position-relative p-3 p-md-5 text-center home-hero shadow-sm">
     <div class="container-fluid">
         <h1 class="hero-title font-weight-normal mt-5 col-lg-8 mx-auto mb-5">Clear data to build the future of psychedelics.</h1>
-        <div class="row mb-5">
-            <div class="col-12 col-md-5 col-lg-6 text-md-right pr-md-5">
-                <img src="{{ asset('images/home-clinical-trial-tracker.png') }}" alt="" style="width: 100%;max-width: 600px;height: auto;">
+        <div class="d-lg-flex justify-content-center">
+            <div class="text-center text-lg-right pr-lg-5">
+                <div id="home-hero-image" class="mx-auto mr-lg-0 mb-3 mb-lg-0">
+                    <img src="{{ asset('images/home-clinical-trial-tracker.png') }}" alt="Clinical Trial Tracker on Neuly" id="first-hero-image" class="active-hero-image">
+                    <img src="{{ asset('images/home-insights.png') }}" alt="Neuly Insights - Analysis on the Psychedelics Industry">
+                    <img src="{{ asset('images/home-investors.png') }}" alt="Neuly's list of investors in the psychedelics industry">
+                    <img src="{{ asset('images/home-research.png') }}" alt="Neuly's list of research in the psychedelics industry">
+                </div>
             </div>
-            <div class="col-12 col-md-7 col-lg-6 text-left">
-                <h2 class="mt-xl-5 h4 font-normal font-weight-normal">Data &amp; Insights for the Psychedelics Industry</h2>
-                <ul>
-                    <li class="lead pt-2 pb-2">Neuly collects and analyzes the mountains of data</li>
-                    <li class="lead pt-2 pb-2">You browse our extensive database and insights</li>
-                    <li class="lead pt-2 pb-2">We all work together to grow the psychedelics industry</li>
-                </ul>
-                <p class="ml-md-3">
-                    <a href="{{ route('member.dashboard') }}" class="btn btn-dark btn-lg">Start exploring our database...</a>
-                </p>
-                <p class="ml-md-3">
-                    <a href="{{ route('about') }}" class="text-dark">Or learn more about Neuly</a>
-                </p>
+            <div class="text-center text-lg-left">
+                <div class="text-left mx-auto d-inline-block">
+                    <h2 class="mt-xl-5 h5 font-normal font-weight-normal d-inline-block">Data &amp; Insights for the Psychedelics Industry</h2>
+                    <ul>
+                        <li class="lead pt-2 pb-2">Neuly collects and analyzes the mountains of data</li>
+                        <li class="lead pt-2 pb-2">You browse our extensive database and insights</li>
+                        <li class="lead pt-2 pb-2">We all work together to grow the psychedelics industry</li>
+                    </ul>
+                    <p class="text-center text-lg-left ml-lg-3">
+                        <a href="{{ route('member.dashboard') }}" class="btn btn-dark btn-lg">Start exploring our database...</a>
+                    </p>
+                    <p class="text-center text-lg-left ml-lg-3">
+                        <a href="{{ route('about') }}" class="text-dark">Or learn more about Neuly</a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
