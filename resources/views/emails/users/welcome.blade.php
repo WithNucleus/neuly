8b99e8c3d57bba500b2ff68211009deb98a17dd3
Sydney Alcala <sydney@gotsmith.com>
2020-11-04 12:14:49 -0600

    Changes to welcome email

--- resources/views/emails/users/welcome.blade.php
+++ resources/views/emails/users/welcome.blade.php
@@ -3,18 +3,23 @@ Hi {{ $name }},
 
 Thanks for becoming our newest Neuly member!
 
-You’ve probably already explored our database, which is the most comprehensive collection of information about the psychedelics industry - that we know of!
+You've probably already explored our database, which is the most comprehensive collection of information about the psychedelics industry - that we know of!
 
-Here are some features you might not have discovered:
+**Here are some features you might not have discovered:**
+
 - You can share information, for free, to anywhere you want. Just use your dashboard to follow entities, write notes, and then share your findings where you want.
-- Need an insight we’re not providing yet? Let us know here. We’ll get on it!
-- Want to add something to our database? Easy - just fill this out.
 
-Want to see other cool stuff you can do with Neuly? Check out our list of Neuly-hacks here.
+- Need an insight we're not providing yet? [Let us know here](https://neuly.test/insights/request). We'll get on it!
+
+- Want to add something to our database? Easy - just [fill this out](https://neuly.test/listing/request).
+
+{{--Want to see other cool stuff you can do with Neuly? Check out our list of Neuly-hacks here.--}}
 
 Have a great week!
 
 Neuly
+
+P.S. Share Neuly with your friends to help us build better transparency and perspective about psychedelics!
 @endcomponent
 
 
