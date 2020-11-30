726eb34ba20c86d65257dd2149a186a505bdf3a2
Sydney Alcala <sydney@gotsmith.com>
2020-11-30 14:52:43 -0600

    Updates to Market Comparison table

--- resources/views/discover/insights/market-comparison/comparison-table.blade.php
+++ resources/views/discover/insights/market-comparison/comparison-table.blade.php
@@ -4,60 +4,63 @@
         <table class="table bg-white mb-0" id="clinical-trial-tracker">
             <thead class="thead-dark">
             <tr>
-                <th class="text-no-wrap sticky-top"></th>
-                <th class="text-no-wrap sticky-top">Name</th>
-                <th class="size-phase text-no-wrap sticky-top">Ownership</th>
+                <th class="text-no-wrap sticky-top">Organization</th>
+                <th class="size-phase text-no-wrap sticky-top">Type</th>
                 <th class="size-phase text-no-wrap sticky-top">Valuation</th>
                 <th class="size-phase text-no-wrap sticky-top">Location</th>
                 <th class="size-phase text-no-wrap sticky-top">Focus</th>
                 <th class="size-phase text-no-wrap sticky-top">People</th>
                 <th class="size-phase text-no-wrap sticky-top">Investors</th>
-                <th class="size-phase text-no-wrap sticky-top">Fouded in</th>
+                <th class="size-phase text-no-wrap sticky-top">Founded in</th>
             </tr>
             </thead>
             <tbody>
             @forelse ($companies as $company)
                 <tr>
-                    <td><img style="width:100px" src="{{ $company->entityImageUrl }}" alt="{{ $company->name }}"/></td>
-                    <td>{{ $company->name}}</td>
-                    <td>{{ $company->ownership }}</td>
-                    <td>
+                    <td class="align-middle">
+                        <div class="d-flex align-items-center">
+                            <div class="logo-is-contained-square flex-shrink-0" style="background-image: url('{{ $company->entityImageUrl }}');"></div>
+                            <strong class="ml-4 font-size-large">{{ $company->name}}</strong>
+                        </div>
+                    </td>
+                    <td class="align-middle">{{ $company->ownership }}</td>
+                    <td class="align-middle">
                         @if(!is_null($company->valuation))
                             {{ $company->valuation }}
                         @else
-                            unknown
+
                         @endif
                     </td>
-                    <td>
+                    <td class="align-middle">
                         @foreach ($company->locations as $location)
                             <a href="{{ route('discover.locations.show', $location->slug) }}">{{ $location->name }}</a> @if (!$loop->last)<br>@endif
                         @endforeach
                     </td>
-                    <td>
+                    <td class="align-middle">
                         @foreach ($company->focus as $item)
                             <a href="{{ route('discover.focus.show', $item->slug) }}">{{ $item->name }}</a>@if (!$loop->last),@endif
                         @endforeach
                     </td>
-                    <td>
+                    <td class="align-middle">
                         @foreach ($company->people as $person)
                             <a href="{{ route('discover.people.show', $person->slug) }}">{{ $person->name }} ({{ $person->pivot->position }})</a> @if (!$loop->last)<br>@endif
                         @endforeach
                     </td>
-                    <td>
+                    <td class="align-middle">
                         @foreach ($company->investors as $investor)
                             <a href="{{ route('discover.investors.show', $investor->slug) }}">{{ $investor->name }}</a> @if (!$loop->last)<br>@endif
                         @endforeach
                     </td>
-                    <td>
+                    <td class="align-middle">
                         @if(!is_null($company->founded_date))
                             {{ $company->founded_date }}
                         @else
-                            unknown
+
                         @endif
                     </td>
                 </tr>
             @empty
-                <td colspan="8">No organisations match your criteria</td>
+                <td colspan="8">No organizations match your criteria</td>
             @endforelse
             </tbody>
         </table>
