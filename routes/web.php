d652b51a2a78485d226994b955e2e6344e03b268
Sydney Alcala <sydney@gotsmith.com>
2020-12-29 13:12:55 -0600

    Add Recruiting Clinical Trial CTA to homepage

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', 'Index\RecruitingClinicalTrialController@index');
-
 // Auth Routes for Front-End
 Route::group(['middleware' => 'firewall.all'], function () {
     Auth::routes(['verify' => true]);
@@ -152,7 +150,7 @@ Route::post('/jobs/apply', 'Index\JobApplicationController@apply')->name('discov
 
 // Clinical trials
 Route::get('/clinical-trials', 'Index\ClinicaltrialController@index')->name('discover.clinicaltrials');
-Route::get('/clinical-trials/recruiting', 'index\RecruitingClinicalTrialController@index')->name('discover.clinicaltrials.recruiting');
+Route::get('/clinical-trials/recruiting', 'Index\RecruitingClinicalTrialController@index')->name('discover.clinicaltrials.recruiting');
 Route::get('/clinical-trials/{slug}', 'Index\ClinicaltrialController@show')->name('discover.clinicaltrials.show');
 
 // Listing Requests
