0464ee0b2dec07d6e35ac0932caef0d7bb8bdb21
Sydney Alcala <sydney@gotsmith.com>
2021-01-27 13:03:16 -0600

    Update Parser to strip attributes from HTML and add summary/description to Clinical Trials show view

--- app/Jobs/Import/ClinicalTrial/ParseDetails.php
+++ app/Jobs/Import/ClinicalTrial/ParseDetails.php
@@ -11,27 +11,36 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use DOMDocument;
+use DOMXPath;
 
 class ParseDetails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     private $baseUrl = 'https://clinicaltrials.gov/ct2/show/';
-
-    /**
-     * @var \App\Models\Clinicaltrial
-     */
     private $clinicaltrial;
+    private $attributesToRemove;
 
     /**
-     * ProcessLocation constructor.
-     * @param \App\Models\Clinicaltrial $clinicaltrial
-     * @param \App\Models\ImportResult $importResult
-     * @param array $locations
+     * Parse Details constructor.
+     * @param Clinicaltrial $clinicaltrial
      */
     public function __construct(Clinicaltrial $clinicaltrial)
     {
         $this->clinicaltrial = $clinicaltrial;
+
+        $this->attributesToRemove = [
+            'class',
+            'style',
+            'lang',
+            'width',
+            'height',
+            'align',
+            'hspace',
+            'vspace',
+            'dir'
+        ];
     }
 
     /**
@@ -48,18 +57,43 @@ class ParseDetails implements ShouldQueue
             $defaultValue = 'Not applicable';
             $url = $this->baseUrl.$this->clinicaltrial->nct_number;
             $crawler = (new Client())->request('GET', $url);
-
             $baseNode = $crawler->filter('#tab-body > div.tr-indent2 > div.tr-indent1 > div.tr-indent2');
-            $parsingResult->brief_summary = $baseNode->filter('.ct-body3.tr-indent2')
+
+            $brief_summary = $baseNode->filter('.ct-body3.tr-indent2')
                 ->first()
-                ->text($defaultValue, true);
-            $parsingResult->detailed_description = $baseNode->filter('#COLLAPSE-DetailedDesc .ct-body3.tr-indent2')
+                ->html($defaultValue);
+
+            $detailedDescription = $baseNode->filter('#COLLAPSE-DetailedDesc .ct-body3.tr-indent2')
                 ->first()
-                ->text($defaultValue, true);
+                ->html($defaultValue);
+
+            $parsingResult->brief_summary = $this->stripAttributes($brief_summary, $this->attributesToRemove);
+            $parsingResult->detailed_description = $this->stripAttributes($detailedDescription, $this->attributesToRemove);
+
         } catch (\Exception $e) {
             Log::error("An error occurred while trying to parse the Clinical trial url: '$url' . Error message: ".$e->getMessage());
         }
 
         $parsingResult->save();
+    }
+
+    /**
+     * Strip Attributes from HTML Strings
+     * @param $html
+     * @param $attributes
+     * @return false|string
+     */
+    private function stripAttributes($html, $attributes) {
+        $html = '<div>' . $html . '</div>';
+        $dom = new DOMDocument;
+        $dom->loadHTML($html);
+        $xPath = new DOMXPath($dom);
+
+        foreach($attributes as $attribute) {
+            $nodes = $xPath->query('//*[@' . $attribute . ']');
+            foreach($nodes as $node) $node->removeAttribute($attribute);
+        }
+
+        return substr($dom->saveHTML($dom->getElementsByTagName('div')->item(0)), 5, -6);
     }
 }
