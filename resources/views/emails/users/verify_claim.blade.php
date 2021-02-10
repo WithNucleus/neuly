510d4652e9acb53578c119c94a92d33546391212
Sydney Alcala <sydney@gotsmith.com>
2021-02-10 12:28:31 -0600

    Changes to Verify Claim email template

--- resources/views/emails/users/verify_claim.blade.php
+++ resources/views/emails/users/verify_claim.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
-    Hi {{ $person_name }},
+Hi {{ $person_name }},
 
-    your person profile at neuly.com was claimed. If the claim was raised by you please verify by clicking on the button.
+Your person listing at neuly.com was claimed. If this was you, please verify by clicking on the button below.
 
-    @component('mail::button', ['url' => route('user.person.verify.email.check', ['token' => $verification_token]), 'color' => 'primary'])
-        verify claim
-    @endcomponent
+@component('mail::button', ['url' => route('user.person.verify.email.check', ['token' => $verification_token]), 'color' => 'primary'])
+verify claim
+@endcomponent
 
-    If you didn't raise this claim please contact us at <a href="{{ route('index') }}">neuly.com</a>.
+If you didn't intend to do this, please contact us at <a href="{{ route('index') }}">neuly.com</a>.
 
-    Best regards,
+Best regards,
 
-    Your team of neuly.com
+Neuly
 @endcomponent
