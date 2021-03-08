bebc4ee054089c6bf8f52e2689d102fbbeb832c3
Marcel Moll <marcel@watson-holmes.com>
2021-03-08 17:47:32 +0100

    adding link to region maps for global map

--- resources/views/discover/organizations/maps/global.blade.php
+++ resources/views/discover/organizations/maps/global.blade.php
@@ -137,6 +137,13 @@
                     label.html(label.html());
                 }
             },
+            onRegionClick: function(event, code){
+                var country = countries[code].country;
+                if(countries[code] !== undefined) {
+                    window.location.href = "/organization/map/" + country;
+
+                }
+            }
         });
     </script>
 @endsection
