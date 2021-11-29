8f9ee6b9cee04296cc255d43b760b092dd2c03b6
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2021-11-29 19:03:11 +0200

    Removed 'auth' middleware from controllers and set it only in routes. Moved custom admin routes to separate file.

--- app/Http/Controllers/Admin/Import/ResultsController.php
+++ app/Http/Controllers/Admin/Import/ResultsController.php
@@ -15,7 +15,6 @@ class ResultsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
         $this->middleware(['role:Admin','permission:import']);
     }
 
--- app/Http/Controllers/Admin/PersonInvestorController.php
+++ app/Http/Controllers/Admin/PersonInvestorController.php
@@ -17,7 +17,6 @@ class PersonInvestorController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
         $this->middleware(['permission:edit investors']);
     }
 
