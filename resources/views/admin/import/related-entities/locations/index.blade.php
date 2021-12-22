942d3f9954e01db90c4485eb41d72c915b743bd2
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2021-12-23 01:47:54 +0200

    Help text updated for Import > Related Entities > Locations.

--- resources/views/admin/import/related-entities/locations/index.blade.php
+++ resources/views/admin/import/related-entities/locations/index.blade.php
@@ -22,7 +22,9 @@
                         </p>
                         <ul>
                             <li>One CSV file per Entity type</li>
-                            <li>Allowed columns: {{ implode(', ', $allowedColumns) }}</li>
+                            <li>Allowed columns: <b>{{ implode(', ', $allowedColumns) }}</b></li>
+                            <li>Location should be in format: <b>Country, State (optional), City (optional)</b>.</li>
+                            <li>Several locations can be separated by "|" sign within one cell.</li>
                         </ul>
 
                         @include('admin.includes.status-messages')
