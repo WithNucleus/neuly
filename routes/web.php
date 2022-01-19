61de8c31d2b89cc20238c1aa3e5c47a6c12ae969
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2022-01-19 04:50:53 +0200

    Embed search: fix missed route.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Dashboard\TeamController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExternalScriptController;
 
 /*
 |--------------------------------------------------------------------------
