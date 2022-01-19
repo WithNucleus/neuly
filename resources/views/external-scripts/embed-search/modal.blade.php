0c86bf737a1f25650ff9a277968c2adfbc6a2cf0
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2022-01-19 08:19:11 +0200

    Embed search: fix CSS styling and filters scripts.

--- resources/views/external-scripts/embed-search/modal.blade.php
+++ resources/views/external-scripts/embed-search/modal.blade.php
@@ -26,58 +26,6 @@ use App\Models\EmbeddableSearchWidget;
                             aria-hidden="true">&#x2715</span></button>
                 </nav>
                 <div class="content">
-{{--                    <div class="dropdown visible-xs">--}}
-{{--                        <button class="btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-{{--                            Menu--}}
-{{--                        </button>--}}
-{{--                        <div class="dropdown-menu">--}}
-{{--                            @foreach($widget->tabs as $tab)--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_MAIN)--}}
-{{--                                    <a class="nav-link nes-main-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-main-section"--}}
-{{--                                       role="tab" aria-controls="main" aria-selected="true">All</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_COMPANIES)--}}
-{{--                                        <a class="nav-link nes-companies-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab"--}}
-{{--                                           href=".nes-section-companies"--}}
-{{--                                           role="tab" aria-controls="companies" aria-selected="false">Organizations</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_PEOPLE)--}}
-{{--                                        <a class="nav-link nes-people-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-section-people"--}}
-{{--                                           role="tab"--}}
-{{--                                           aria-controls="people" aria-selected="false">People</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_INVESTORS)--}}
-{{--                                        <a class="nav-link nes-investors-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab"--}}
-{{--                                           href=".nes-section-investors"--}}
-{{--                                           role="tab" aria-controls="investors" aria-selected="false">Investors</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_RESEARCH)--}}
-{{--                                        <a class="nav-link nes-research-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-section-research"--}}
-{{--                                           role="tab" aria-controls="research" aria-selected="false">Research</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_CLINICAL_TRIALS)--}}
-{{--                                        <a class="nav-link nes-clinical-trials-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab"--}}
-{{--                                           href=".nes-section-clinical-trials" role="tab" aria-controls="clinical-trials"--}}
-{{--                                           aria-selected="false">Clinical Trials</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_EVENTS)--}}
-{{--                                        <a class="nav-link nes-events-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-section-events"--}}
-{{--                                           role="tab"--}}
-{{--                                           aria-controls="events" aria-selected="false">Events</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_JOBS)--}}
-{{--                                        <a class="nav-link nes-jobs-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-section-jobs"--}}
-{{--                                           role="tab"--}}
-{{--                                           aria-controls="jobs" aria-selected="false">Jobs</a>--}}
-{{--                                @endif--}}
-{{--                                @if($tab == EmbeddableSearchWidget::TAB_NEWS_ARTICLES)--}}
-{{--                                        <a class="nav-link nes-news-articles-tab {{ $activeTab == $tab ? 'active' : '' }}" data-toggle="tab" href=".nes-section-news-articles"--}}
-{{--                                           role="tab"--}}
-{{--                                           aria-controls="news-articles" aria-selected="false">News Articles</a>--}}
-{{--                                @endif--}}
-{{--                            @endforeach--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
                     <ul class="nav nav-tabs mb-2" role="tablist">
                         @foreach($widget->tabs as $tab)
                             @if($tab == EmbeddableSearchWidget::TAB_MAIN)
@@ -161,11 +109,11 @@ use App\Models\EmbeddableSearchWidget;
                                         @endif
 {{--                                        @include('external-scripts.embed-search.includes.filters', [--}}
 {{--                                            'filters' => [--}}
-{{--                                                /*'has-events' => true,--}}
+{{--                                                'has-events' => true,--}}
 {{--                                                'has-jobs' => true,--}}
 {{--                                                'type' => true,--}}
 {{--                                                'countries' => true,--}}
-{{--                                                'locations' => true,*/--}}
+{{--                                                'locations' => true,--}}
 {{--                                                'focus' => true,--}}
 {{--                                            ]--}}
 {{--                                        ])--}}
@@ -185,7 +133,7 @@ use App\Models\EmbeddableSearchWidget;
 {{--                                        @include('external-scripts.embed-search.includes.filters', [--}}
 {{--                                            'filters' => [--}}
 {{--                                                'focus' => true,--}}
-{{--                                                /*'countries' => true,*/--}}
+{{--                                                'countries' => true,--}}
 {{--                                            ]--}}
 {{--                                        ])--}}
                                         @include('external-scripts.embed-search.includes.data-section', ['title' => 'People'])
@@ -238,7 +186,8 @@ use App\Models\EmbeddableSearchWidget;
                                     <div class="row search-content-row">
                                         @if(isset($widget->filters[EmbeddableSearchWidget::TAB_CLINICAL_TRIALS]))
                                             @include('external-scripts.embed-search.includes.filters', [
-                                                'filters' => $widget->filters[EmbeddableSearchWidget::TAB_CLINICAL_TRIALS]
+                                                'filters' => $widget->filters[EmbeddableSearchWidget::TAB_CLINICAL_TRIALS],
+                                                'labels' => ['people' => 'Researches']
                                             ])
                                         @endif
 {{--                                        @include('external-scripts.embed-search.includes.filters', [--}}
@@ -280,7 +229,8 @@ use App\Models\EmbeddableSearchWidget;
                                     <div class="row search-content-row">
                                         @if(isset($widget->filters[EmbeddableSearchWidget::TAB_JOBS]))
                                             @include('external-scripts.embed-search.includes.filters', [
-                                                'filters' => $widget->filters[EmbeddableSearchWidget::TAB_JOBS]
+                                                'filters' => $widget->filters[EmbeddableSearchWidget::TAB_JOBS],
+                                                 'labels' => ['companies' => 'Owner']
                                             ])
                                         @endif
 {{--                                        @include('external-scripts.embed-search.includes.filters', [--}}
