02e133e44fb9f43a88b61e472d028fbc3d6b6201
Marcel Moll <marcel@watson-holmes.com>
2022-02-09 19:06:04 +0100

    fixing problems with 404 by disbabling multiple inputs while loading

--- resources/views/sidebars/filters/scripts.blade.php
+++ resources/views/sidebars/filters/scripts.blade.php
@@ -62,6 +62,8 @@
             'year',
         ];
 
+        $('#filterSidebar input').prop('disabled', true);
+
         allowedFilters.forEach(function (filterName) {
             let filters = getFilterValuesByName(filterName);
 
@@ -88,6 +90,7 @@
         }
 
         // Redirect
+
         document.location.href = url;
     }
 
