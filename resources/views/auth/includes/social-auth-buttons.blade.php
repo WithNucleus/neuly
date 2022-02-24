3afcf9e141066d593a06bdcab1717b23c8e48b79
Sydney Alcala <sydney@gotsmith.com>
2022-02-24 15:43:52 -0600

    Add Twitter login back

--- resources/views/auth/includes/social-auth-buttons.blade.php
+++ resources/views/auth/includes/social-auth-buttons.blade.php
@@ -4,6 +4,9 @@
 <a href="{{ route('login.social', 'facebook') }}" class="btn btn-primary btn-sm m-1">
     <i class="fab fa-facebook-f"></i> Facebook
 </a>
+<a href="{{ route('login.social', 'twitter') }}" class="btn btn-twitter text-white btn-sm m-1">
+    <i class="fab fa-twitter"></i> Twitter
+</a>
 <a href="{{ route('login.social', 'linkedin') }}" class="btn btn-primary btn-sm m-1">
     <i class="fab fa-linkedin"></i> LinkedIn
 </a>
