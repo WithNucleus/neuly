147da6552bd387eb11026ecfbbaae4fa7654e2f4
Marcel Moll <marcel@watson-holmes.com>
2022-03-02 18:45:57 +0100

    fixing logo path for emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://neuly.com/images/neuly-logo-light.png" class="logo" alt="{{ config('app.name', 'Neuly') }}">
+<img src="https://app.neuly.com/images/neuly-logo-light.png" class="logo" alt="{{ config('app.name', 'Neuly') }}">
 </a>
 </td>
 </tr>
