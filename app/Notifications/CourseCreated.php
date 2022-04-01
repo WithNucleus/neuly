d77aff9985129c52cbde89fcb7208c2c01bf03e3
Marcel Moll <marcel@watson-holmes.com>
2022-04-01 19:58:05 +0200

    fixing bug with name for courses created

--- app/Notifications/CourseCreated.php
+++ app/Notifications/CourseCreated.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CourseCreated.php extends Notification
+class CourseCreated extends Notification
 {
     use Queueable;
 
