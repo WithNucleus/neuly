26898d3ae5828994766e40898d612e6bd1a23edd
Sydney Alcala <sydney@gotsmith.com>
2022-07-20 10:29:44 -0500

    Fixes for AutoSuggest

--- app/Search/AutoSuggest.php
+++ app/Search/AutoSuggest.php
@@ -3,8 +3,10 @@
 namespace App\Search;
 
 use Algolia\ScoutExtended\Searchable\Aggregator;
+use App\Enum\MediaTypes;
 use App\Models\BookableListing;
 use App\Models\Course;
+use App\Models\DataFeed;
 use App\Models\MediaItem;
 use \App\Models\Company;
 use \App\Models\Person;
@@ -58,41 +60,49 @@ class AutoSuggest extends Aggregator
 
     private function getUrl($model): string
     {
-        return match (class_basename($model)) {
-            'Company', => route('discover.organizations.show', $model->slug),
-            'Person', => route('discover.people.show', $model->slug),
-            'Investor', => route('discover.investors.show', $model->slug),
-            'Research', => route('discover.research.show', $model->slug),
-            'Clinicaltrial', => route('discover.clinicaltrials.show', $model->slug),
-            'Event', => route('discover.events.show', $model->slug),
-            'Job', => route('discover.jobs.show', $model->slug),
-            'MediaItem', 'Course', => $model->url,
-            'BookableListing', => route('discover.bookable-listing.show', $model->slug),
+        return match (get_class($model)) {
+            Company::class => route('discover.organizations.show', $model->slug),
+            Person::class => route('discover.people.show', $model->slug),
+            Investor::class => route('discover.investors.show', $model->slug),
+            Research::class => route('discover.research.show', $model->slug),
+            Clinicaltrial::class, => route('discover.clinicaltrials.show', $model->slug),
+            Event::class => route('discover.events.show', $model->slug),
+            Job::class => route('discover.jobs.show', $model->slug),
+            MediaItem::class, 'Course' => $model->url,
+            BookableListing::class => route('discover.bookable-listing.show', $model->slug),
             default => url('')
         };
     }
 
     private function getModelType($model): string
     {
-        return match (class_basename($model)) {
-            'Company', => 'Organization',
-            'Clinicaltrial', => 'Clinical Trial',
-            'MediaItem' => $model->media_type,
-            'BookableListing' => $model->type,
+        $modelType = match (get_class($model)) {
+            Company::class => 'Organization',
+            Clinicaltrial::class => 'Clinical Trial',
+            MediaItem::class => $model->media_type,
+            BookableListing::class => $model->type,
             default => class_basename($model)
         };
+
+        if (get_class($model) === MediaItem::class) {
+            if ($model->source) {
+                if ($model->source->source_category !== DataFeed::SOURCE_GOOGLE_ALERT) {
+                    $modelType .= ' - ' . $model->source->name;
+                }
+            }
+        }
+
+        return $modelType;
     }
 
     private function getKeywords($model): string
     {
-        // TODO: Also for Media Items -- add plural type to model
-
-        $type = match (class_basename($model)) {
-            'Company', => 'organizations ' . $model->ownership,
-            'Clinicaltrial', => 'clinical trials',
-            'Person', => 'people person',
-            'MediaItem' => Str::plural($model->media_type),
-            'BookableListing' => Str::plural($model->type),
+        $type = match (get_class($model)) {
+            Company::class => 'organizations',
+            Clinicaltrial::class => 'clinical trials',
+            Person::class => 'people person',
+            MediaItem::class => Str::plural($model->media_type),
+            BookableListing::class => Str::plural($model->type),
             default => class_basename($model)
         };
 
@@ -112,11 +122,19 @@ class AutoSuggest extends Aggregator
             }
         }
 
-        if (class_basename($model) === 'BookableListing') {
+        if (get_class($model) === Company::class) {
+            $keywords .= $model->ownership . " ";
+        }
+
+        if (get_class($model) === Investor::class) {
+            $keywords .= $model->type . " ";
+        }
+
+        if (get_class($model) === BookableListing::class) {
             $keywords .= $model->location_name . " ";
         }
 
-        if (class_basename($model) === 'Job') {
+        if (get_class($model) === Job::class) {
             $keywords .= $model->employment_type . " at " . $model->owner->name . " ";
         }
 
@@ -126,9 +144,7 @@ class AutoSuggest extends Aggregator
     private function getDescription($model): string
     {
         // Organization
-        if (class_basename($model) === 'Company') {
-
-            // TODO: Entity Content
+        if (get_class($model) === Company::class) {
 
             if ($model->ownership) {
                 return $model->ownership;
@@ -136,17 +152,17 @@ class AutoSuggest extends Aggregator
         }
 
         // Person
-        if (class_basename($model) === 'Person') {
+        if (get_class($model) === Person::class) {
             return strip_tags($model->byline ?? $model->bio);
         }
 
         // Investor
-        if (class_basename($model) === 'Investor') {
+        if (get_class($model) === Investor::class) {
             return $model->type;
         }
 
         // Research
-        if (class_basename($model) === 'Research') {
+        if (get_class($model) === Research::class) {
             $description = '';
 
             if ($model->publication_info) {
@@ -165,33 +181,39 @@ class AutoSuggest extends Aggregator
         }
 
         // Clinical Trial
-        if (class_basename($model) === 'Clinicaltrial') {
+        if (get_class($model) === Clinicaltrial::class) {
             return $model->nct_number . ' ' . strip_tags($model->brief_summary);
         }
 
         // Event
-        if (class_basename($model) === 'Event') {
+        if (get_class($model) === Event::class) {
             return strip_tags(html_entity_decode($model->description));
         }
 
         // Job
-        if (class_basename($model) === 'Job') {
+        if (get_class($model) === Job::class) {
             return strip_tags(html_entity_decode($model->job_description));
         }
 
         // MediaItem
-        if (class_basename($model) === 'MediaItem') {
+        if (get_class($model) === MediaItem::class) {
             return strip_tags(html_entity_decode($model->summary ?? $model->content));
         }
 
         // Course
-        if (class_basename($model) === 'MediaItem') {
+        if (get_class($model) === MediaItem::class) {
             return strip_tags($model->summary);
         }
 
         // BookableListing
-        if (class_basename($model) === 'BookableListing') {
-            // TODO: Entity Content
+        if (get_class($model) === BookableListing::class) {
+
+            $firstContent = $model->content()->first();
+
+            if ($firstContent) {
+                return strip_tags($firstContent);
+            }
+
             return 'Bookable ' . $model->type;
         }
 
@@ -201,16 +223,21 @@ class AutoSuggest extends Aggregator
 
     private function getImage($model): string
     {
-        $image = match (class_basename($model)) {
-            'Company', 'Investor', 'Person', 'Event', =>  $model->entityImageUrl,
-            'Research', => '/images/icons/research.svg',
-            'Clinicaltrial', => '/images/icons/clinicaltrials.svg',
-            'Job', => '/images/icons/jobs.svg',
+        $image = match (get_class($model)) {
+            Company::class, Investor::class, Person::class, Event::class =>  $model->entityImageUrl,
+            Research::class => '/images/icons/research.svg',
+            Clinicaltrial::class => '/images/icons/clinicaltrials.svg',
+            Job::class => '/images/icons/jobs.svg',
+            MediaItem::class => $model->icon_url,
             default => '/images/favicons/android-chrome-192x192.png'
         };
 
         if (!$image) {
             return config('scout.image_url_prefix') . '/images/favicons/android-chrome-192x192.png';
+        }
+
+        if (get_class($model) === MediaItem::class) {
+            return $image;
         }
 
         return config('scout.image_url_prefix') . $image;
