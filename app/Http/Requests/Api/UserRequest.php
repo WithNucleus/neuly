aa4f6c8b8b7994a40d2a38a07569539a83a0239d
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2022-07-25 19:56:24 +0300

    NEULY-232 Added endpoints to manage users and their roles.

---
+++ app/Http/Requests/Api/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->hasWritePermission();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if (in_array($this->method(), ['PUT', 'PATCH'])) {
+            return [
+                'name' => ['string', 'min:2', 'max:255'],
+                'last_name' => ['string', 'min:2', 'max:255'],
+                'email' => ['string', 'email', 'max:255', 'unique:users'],
+            ];
+        }
+
+        return [
+            'name' => ['required', 'string', 'min:2', 'max:255'],
+            'last_name' => ['required', 'string', 'min:2', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'role' => ['required', 'exists:roles,name'],
+        ];
+    }
+}
