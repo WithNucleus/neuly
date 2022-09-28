0d59c324f4a16e30de8a5b16c0335879bb67e491
vsdoroshenko <vsdoroshenko.dev@gmail.com>
2022-09-28 22:28:36 +0300

    NEULY-224 Refactored Locations search on index pages. Added new Locations search for People and Investors index pages.

--- resources/views/sidebars/companies.blade.php
+++ resources/views/sidebars/companies.blade.php
@@ -16,26 +16,11 @@
     'item_filters' => $filters_type
 ])
 
-<div class="organizations-locations mb-4">
-    <label for="locations" class="h4">Locations</label>
-    <div class="d-flex">
-        <input type="text" class="typeahead form-control" name="locations-search" placeholder="e.g. New York">
-        <button class="btn btn-link px-1 text-primary"><i class="fad fa-search fa-lg"></i></button>
-    </div>
-
-    <div id="locations-filter">
-        <span class="d-block title"></span>
-
-        @isset($filters_location)
-            @foreach ($filters_location as $location)
-                <div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="locations" id="{{ $location }}" value="{{ $location }}" checked>
-                    <label class="custom-control-label" for="{{ $location }}">{{ $location }}</label>
-                </div>
-            @endforeach
-        @endisset
-    </div>
-</div>
+@include('sidebars.filters.locations', [
+    'label' => 'Locations',
+    'filters_location' => $filters_location,
+    'actionUrl' => route('searchassets.companiesLocations'),
+])
 
 @include('sidebars.filters.checkboxes-new', [
     'label'     => 'Focus',
--- resources/views/sidebars/locations.blade.php
+++ resources/views/sidebars/locations.blade.php
@@ -1,33 +1,8 @@
-<div class="locations-locations mb-4">
-	<label for="people" class="h4">Search</label>
-	<div class="d-flex">
-		<input type="text" class="typeahead form-control" name="locations-search" placeholder="e.g. California">
-		<button class="btn btn-link px-1 text-primary"><i class="fad fa-search fa-lg"></i></button>
-	</div>
-
-	<div id="locations-filter">
-		<span class="d-block title"></span>
-
-		@isset($filters_location)
-			@foreach ($filters_location as $location)
-				<div class="custom-control custom-checkbox">
-                    <input type="checkbox" class="custom-control-input" name="locations" id="{{ $location }}" value="{{ $location }}" checked>
-                    <label class="custom-control-label" for="{{ $location }}">{{ $location }}</label>
-                </div>
-			@endforeach
-		@endisset
-	</div>
-</div>
-
-{{-- @include('sidebars.filters.checkboxes-new', [
-    'label'     => 'Countries',
-    'name'      => 'country',
-    'items'     =>  [
-    					'USA' => 'USA',
-    					'UK' => 'UK'
-    				],
-    'item_filters' => $filters_location
-]) --}}
+@include('sidebars.filters.locations', [
+    'label' => 'Search',
+    'filters_location' => $filters_location,
+    'actionUrl' => route('searchassets.locationRegions')
+])
 
 @include('sidebars.filters.checkboxes-new', [
     'label'     => 'Countries',
