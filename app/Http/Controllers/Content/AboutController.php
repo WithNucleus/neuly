a9fc9a1f36ba52495e5ca71cbc1e6979335ca0ee
Sydney Alcala <sydney@gotsmith.com>
2023-04-13 12:02:22 -0500

    Add Chatbase Widget and data to about page

--- app/Http/Controllers/Content/AboutController.php
+++ app/Http/Controllers/Content/AboutController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers\Content;
 
 use App\Http\Controllers\Controller;
+use App\Models\BookableListing;
+use App\Models\Clinicaltrial;
+use App\Models\Company;
+use App\Models\Course;
+use App\Models\Event;
+use App\Models\Investor;
+use App\Models\Job;
+use App\Models\Location;
+use App\Models\MediaItem;
+use App\Models\Person;
+use App\Models\Research;
 use App\Services\Metas;
 use Illuminate\Http\Request;
 
@@ -13,6 +24,37 @@ class AboutController extends Controller
         // Metas
         $metas = Metas::fromPage($request->path());
 
-        return view('content.about.index', compact('metas'));
+        $organizationsCount = Company::count();
+        $peopleCount = Person::count();
+        $investorsCount = Investor::count();
+        $locationsCount = Location::count();
+        $researchCount = Research::count();
+        $clinicalTrialsCount = Clinicaltrial::count();
+        $eventsCount = Event::count();
+        $jobsCount = Job::count();
+        $newsCount = MediaItem::news()->count();
+        $booksCount = MediaItem::books()->count();
+        $podcastsCount = MediaItem::podcasts()->count();
+        $coursesCount = Course::count();
+        $videosCount = MediaItem::videos()->count();
+        $careCount = BookableListing::practitioners()->public()->count();
+
+        return view('content.about.index', [
+            'metas' => $metas,
+            'organizationsCount' => $organizationsCount,
+            'peopleCount' => $peopleCount,
+            'investorsCount' => $investorsCount,
+            'locationsCount' => $locationsCount,
+            'researchCount' => $researchCount,
+            'clinicalTrialsCount' => $clinicalTrialsCount,
+            'eventsCount' => $eventsCount,
+            'jobsCount' => $jobsCount,
+            'newsCount' => $newsCount,
+            'booksCount' => $booksCount,
+            'podcastsCount' => $podcastsCount,
+            'coursesCount' => $coursesCount,
+            'videosCount' => $videosCount,
+            'careCount' => $careCount
+        ]);
     }
 }
