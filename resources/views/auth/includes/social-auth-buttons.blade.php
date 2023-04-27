6a30aee58b81289627fcbb45550ca18a179dd495
Sydney Alcala <sydney@gotsmith.com>
2023-04-27 09:46:45 -0500

    Remove Twitter login

--- resources/views/auth/includes/social-auth-buttons.blade.php
+++ resources/views/auth/includes/social-auth-buttons.blade.php
@@ -1,12 +1,9 @@
-<a href="{{ route('login.social', 'google') }}" class="btn btn-danger btn-sm m-1">
+<a href="{{ route('login.social', 'google') }}" class="btn btn-danger btn-sm m-1" style="background-color: #DB4437;border-color: #DB4437">
     <i class="fab fa-google"></i> Google
 </a>
-<a href="{{ route('login.social', 'facebook') }}" class="btn btn-primary btn-sm m-1">
+<a href="{{ route('login.social', 'facebook') }}" class="btn btn-primary btn-sm m-1" style="background-color: #4267B2;border-color: #4267B2">
     <i class="fab fa-facebook-f"></i> Facebook
 </a>
-<a href="{{ route('login.social', 'twitter') }}" class="btn btn-twitter text-white btn-sm m-1">
-    <i class="fab fa-twitter"></i> Twitter
-</a>
-<a href="{{ route('login.social', 'linkedin') }}" class="btn btn-primary btn-sm m-1">
+<a href="{{ route('login.social', 'linkedin') }}" class="btn btn-primary btn-sm m-1" style="background-color: #0077b5;border-color: #0077b5">
     <i class="fab fa-linkedin"></i> LinkedIn
 </a>
