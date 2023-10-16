a237832913666a68976d7e0feab49a68241a10bf
Sydney Alcala <sydney@gotsmith.com>
2023-10-16 11:28:31 -0500

    Editing email templates

--- resources/views/adminx/emails/templates/show.blade.php
+++ resources/views/adminx/emails/templates/show.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <div class="container-fluid my-4">
-        <h1 class="mb-5">Email Template: {{ $template->name }}</h1>
-        <x-admin.emails.email-template-details :emailTemplate="$template" />
+        <livewire:admin.emails.templates.manage-template :template="$template" />
     </div>
 @endsection
